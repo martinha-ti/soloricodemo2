a01392508b0b399a957cb27ee1541105d63d668e
Martinha TI <7593089+martinha-ti@users.noreply.github.com>
2025-02-20 22:02:50 -0300

    Add files via upload

---
+++ testes/slider.js
@@ -0,0 +1,5 @@
+var myCarousel = document.querySelector('#carouselExample');
+var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 5000, // 5000ms = 5 segundos
+    ride: 'carousel'
+});
